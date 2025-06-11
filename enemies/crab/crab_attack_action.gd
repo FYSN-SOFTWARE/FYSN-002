466653ebde5d4042ec5bf9f146ced8514c5f228c
www55111 <563568669@qq.com>
2025-06-11 23:07:43 +0800

    Add files via upload

--- enemies/crab/crab_attack_action.gd
+++ enemies/crab/crab_attack_action.gd
@@ -2,8 +2,15 @@ extends EnemyAction
 
 @export var damage := 7
 
+func perform_normal_action() -> void:
+	_do_attack(damage)
 
-func perform_action() -> void:
+func perform_flipped_action() -> void:
+	# 里世界伤害加成
+	var flipped_damage = damage * enemy.stats.damage_multiplier
+	_do_attack(flipped_damage)
+
+func _do_attack(attack_damage: int) -> void:
 	if not enemy or not target:
 		return
 	
@@ -12,7 +19,7 @@ func perform_action() -> void:
 	var end := target.global_position + Vector2.RIGHT * 32
 	var damage_effect := DamageEffect.new()
 	var target_array: Array[Node] = [target]
-	damage_effect.amount = damage
+	damage_effect.amount = attack_damage
 	damage_effect.sound = sound
 	
 	tween.tween_property(enemy, "global_position", end, 0.4)
@@ -31,5 +38,10 @@ func update_intent_text() -> void:
 	if not player:
 		return
 	
-	var modified_dmg := player.modifier_handler.get_modified_value(damage, Modifier.Type.DMG_TAKEN)
+	# 考虑里世界伤害加成
+	var base_damage = damage
+	if enemy and enemy.is_flipped:
+		base_damage *= enemy.stats.damage_multiplier
+	
+	var modified_dmg := player.modifier_handler.get_modified_value(base_damage, Modifier.Type.DMG_TAKEN)
 	intent.current_text = intent.base_text % modified_dmg
