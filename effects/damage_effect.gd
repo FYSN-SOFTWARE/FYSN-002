cf99b12b666c9f7a2882a9ae9fbc5e1b107d39c9
www55111 <563568669@qq.com>
2025-06-11 23:05:38 +0800

    Add files via upload

--- effects/damage_effect.gd
+++ effects/damage_effect.gd
@@ -7,8 +7,6 @@ var receiver_modifier_type := Modifier.Type.DMG_TAKEN
 
 func execute(targets: Array[Node]) -> void:
 	for target in targets:
-		if not target:
-			continue
 		if target is Enemy or target is Player:
-			target.take_damage(amount, receiver_modifier_type)
+			target.take_damage(int(amount), receiver_modifier_type)
 			SFXPlayer.play(sound)
