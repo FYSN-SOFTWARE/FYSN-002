f256feeea4677364d03ea12afbe687c51b1c1deb
www55111 <563568669@qq.com>
2025-06-11 23:08:34 +0800

    Add files via upload

--- enemies/toxic_ghost/toxic_ghost_attack_action.gd
+++ enemies/toxic_ghost/toxic_ghost_attack_action.gd
@@ -4,8 +4,15 @@ const TOXIN = preload("res://common_cards/toxin.tres")
 
 @export var damage := 8
 
+func perform_normal_action() -> void:
+	_do_attack(damage)
 
-func perform_action() -> void:
+func perform_flipped_action() -> void:
+	# 里世界伤害加成
+	var flipped_damage = damage * enemy.stats.damage_multiplier
+	_do_attack(flipped_damage)
+
+func _do_attack(attack_damage: int) -> void:
 	if not enemy or not target:
 		return
 	
@@ -18,8 +25,7 @@ func perform_action() -> void:
 	var end := target.global_position + Vector2.RIGHT * 32
 	var damage_effect := DamageEffect.new()
 	var target_array: Array[Node] = [target]
-	var modified_dmg := enemy.modifier_handler.get_modified_value(damage, Modifier.Type.DMG_DEALT)
-	
+	var modified_dmg := enemy.modifier_handler.get_modified_value(attack_damage, Modifier.Type.DMG_DEALT)
 	damage_effect.amount = modified_dmg
 	damage_effect.sound = sound
 	
@@ -39,8 +45,11 @@ func update_intent_text() -> void:
 	var player := target as Player
 	if not player:
 		return
-	
-	var modified_dmg := player.modifier_handler.get_modified_value(damage, Modifier.Type.DMG_TAKEN)
+	# 考虑里世界伤害加成
+	var base_damage = damage
+	if enemy and enemy.is_flipped:
+		base_damage *= enemy.stats.damage_multiplier
+
+	var modified_dmg := player.modifier_handler.get_modified_value(base_damage, Modifier.Type.DMG_TAKEN)
 	var final_dmg := enemy.modifier_handler.get_modified_value(modified_dmg, Modifier.Type.DMG_DEALT)
 	intent.current_text = intent.base_text % final_dmg
-
