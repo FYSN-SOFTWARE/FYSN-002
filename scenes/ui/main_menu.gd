deb80325d72b89552562df37d24c40da0325dd30
Faith <suckunityhub@163.com>
2025-06-25 22:33:51 +0800

    实现主界面UI替换

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -4,9 +4,14 @@ const CHAR_SELECTOR_SCENE := preload("res://scenes/ui/character_selector.tscn")
 const RUN_SCENE = preload("res://scenes/run/run.tscn")
 
 @export var run_startup: RunStartup
-
 @onready var continue_button: Button = %Continue
+@onready var new_run_button: Button = %NewRun
+@onready var exit_button: Button = %Exit
 
+@export var hover_scale: Vector2 = Vector2(1.1, 1.1)
+@export var hover_duration: float = 0.2
+@export var hover_color: Color = Color(0.9, 0.9, 1.0)
+var normal_color: Color
 
 func _ready() -> void:
 	get_tree().paused = false
