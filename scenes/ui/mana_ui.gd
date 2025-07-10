f008c02e46e6cde4a09bed9f67e97fc692a68368
TheMoonToWest <xiayuanping12138@163.com>
2025-07-10 21:40:16 +0800

    费用

--- scenes/ui/mana_ui.gd
+++ scenes/ui/mana_ui.gd
@@ -4,19 +4,17 @@ extends Panel
 @export var char_stats: CharacterStats : set = _set_char_stats
 
 @onready var mana_label: Label = $ManaLabel
+@onready var san_label: Label = $SanLabel
 
+var mana : int 
+var max_mana : int
+var soals : int
+var max_soals : int
 
 func _set_char_stats(value: CharacterStats) -> void:
 	char_stats = value
-	
-	if not char_stats.stats_changed.is_connected(_on_stats_changed):
-		char_stats.stats_changed.connect(_on_stats_changed)
-
-	if not is_node_ready():
-		await ready
-
-	_on_stats_changed()
 
 
-func _on_stats_changed() -> void:
-	mana_label.text = "%s/%s" % [char_stats.mana, char_stats.max_mana]
+func text_change() -> void:
+	mana_label.text = "%s/%s" % [mana, max_mana]
+	san_label.text = "%s/%s" % [soals, max_soals]
