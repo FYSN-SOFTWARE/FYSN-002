6b4d7406c820ec6740fa80b18c6b268247e88418
TheMoonToWest <xiayuanping12138@163.com>
2025-07-13 15:02:16 +0800

    testcard

--- custom_resources/card.gd
+++ custom_resources/card.gd
@@ -11,85 +11,107 @@ const RARITY_COLORS := {
 	Card.Rarity.LEGEND: Color.RED
 }
 
-@export var id: String:
-	get:
-		if (isBackworld):
-			return otherbackcard.id
-		return outcard.id
-@export var type: Type:
-	get:
-		if (isBackworld):
-			return otherbackcard.type
-		return outcard.type
-@export var rarity: Rarity:
-	get:
-		if (isBackworld):
-			return otherbackcard.rarity
-		return outcard.rarity
-@export var target: Effect.Target:
-	get:
-		if (isBackworld):
-			return otherbackcard.target
-		return outcard.target
-@export var cost: int:
-	get:
-		if (isBackworld):
-			return otherbackcard.cost
-		return outcard.cost
+# 默认卡牌面配置
+const DEFAULT_CARD_SIDE := {
+	"id": "default_card",
+	"type": Type.ATTACK,
+	"rarity": Rarity.COMMON,
+	"target": Effect.Target.SINGLE_ENEMY,
+	"cost": 1,
+	"icon": null,
+	"tooltip_text": "Default Card"
+}
 
-@export_group("Card Visuals")
-@export var icon: Texture:
-	get:
-		if (isBackworld):
-			return otherbackcard.icon
-		return outcard.icon
-@export var tooltip_text: String:
-	get:
-		if (isBackworld):
-			return otherbackcard.id
-		return outcard.id
-@export var tooltips: Array[CardTooltip]:
-	get:
-		if (isBackworld):
-			return otherbackcard.tooltips
-		return outcard.tooltips
-		
-		
-@export var outcard: CardSide
-@export var backcard: CardSide
+# 导出变量组
+@export_group("Card References")
+@export var outcard: CardSide:
+	set(value):
+		outcard = value if value else _create_default_side()
+@export var backcard: CardSide:
+	set(value):
+		backcard = value if value else _create_default_side()
+		otherbackcard = backcard
 @export var sound: AudioStream
 
-# 添加升级属性
-var upgraded: bool = false
+@export_group("Upgrade Properties")
 @export var upgrade_damage: int = 2
 @export var upgrade_block: int = 2
 @export var upgrade_cost: int = 0
 @export_multiline var upgraded_tooltip_text: String
 
-var otherbackcard: CardSide = backcard
+@export_group("Tooltips")
+@export var tooltips: Array[CardTooltip]
 
-var isBackworld :bool = false
+# 运行时变量
+var otherbackcard: CardSide
+var isBackworld: bool = false
 var cardarea: Effect.CardArea = 0
+var upgraded: bool = false
 
-#进入战斗时需要触发
+func _ready():
+	# 确保卡牌面初始化
+	if not outcard:
+		outcard = _create_default_side()
+	if not backcard:
+		backcard = _create_default_side()
+	otherbackcard = backcard
+
+# 创建默认卡牌面
+func _create_default_side() -> CardSide:
+	var side = CardSide.new()
+	side.id = DEFAULT_CARD_SIDE["id"]
+	side.type = DEFAULT_CARD_SIDE["type"]
+	side.target = DEFAULT_CARD_SIDE["target"]
+	side.cost = DEFAULT_CARD_SIDE["cost"]
+	side.icon = DEFAULT_CARD_SIDE["icon"]
+	return side
+
+# 安全属性访问器
+func get_current_side() -> CardSide:
+	return otherbackcard if isBackworld else outcard
+
+# 属性访问
+@export var id: String:
+	get: return get_current_side().id if get_current_side() else DEFAULT_CARD_SIDE["id"]
+
+@export var type: Type:
+	get: return get_current_side().type if get_current_side() else DEFAULT_CARD_SIDE["type"]
+
+@export var rarity: Rarity:
+	get: return get_current_side().rarity if get_current_side() else DEFAULT_CARD_SIDE["rarity"]
+
+@export var target: Effect.Target:
+	get: return get_current_side().target if get_current_side() else DEFAULT_CARD_SIDE["target"]
+
+@export var cost: int:
+	get: return get_current_side().cost if get_current_side() else DEFAULT_CARD_SIDE["cost"]
+	set(value):
+		if get_current_side():
+			get_current_side().cost = value
+
+@export var icon: Texture:
+	get: return get_current_side().icon if get_current_side() else DEFAULT_CARD_SIDE["icon"]
+
+@export var tooltip_text: String:
+	get: return get_current_side().id if get_current_side() else DEFAULT_CARD_SIDE["id"]
+
+# 战斗相关方法
 func initiative() -> void:
-	effects = outcard.effects.duplicate()
+	if outcard:
+		effects = outcard.effects.duplicate()
 	Events.world_flipped.connect(on_worldflip)
 
-#反转时触发
 func on_worldflip(flipword: bool) -> void:
 	isBackworld = flipword
 
 func is_single_targeted() -> bool:
 	return target == Effect.Target.SINGLE_ENEMY
 
-
 func _get_targets(targets: Array[Node]) -> Array[Node]:
 	if not targets:
 		return []
-		
-	var tree := targets[0].get_tree()
 	
+	var tree := targets[0].get_tree()
 	match target:
 		Effect.Target.SELF:
 			return tree.get_nodes_in_group("player")
@@ -100,8 +122,11 @@ func _get_targets(targets: Array[Node]) -> Array[Node]:
 		_:
 			return []
 
-
 func play(targets: Array[Node], char_stats: CharacterStats, modifiers: ModifierHandler) -> void:
+	if not get_current_side():
+		push_error("Card side is invalid!")
+		return
+	
 	Events.card_played.emit(self)
 	char_stats.mana -= cost
 	
@@ -110,32 +135,23 @@ func play(targets: Array[Node], char_stats: CharacterStats, modifiers: ModifierH
 	else:
 		apply_effects(_get_targets(targets), modifiers)
 
-
-# 升级卡牌
+# 升级系统
 func upgrade() -> void:
 	upgraded = true
-	
-	# 调用子类特定的升级逻辑
 	post_upgrade()
-	
-	# 发出卡牌升级事件
 	Events.card_upgraded.emit(self)
 
-# 添加这个方法允许子类自定义升级逻辑
 func post_upgrade() -> void:
-	# 默认实现为空，子类可以覆盖
 	pass
 
 func apply_effects(_targets: Array[Node], _modifiers: ModifierHandler) -> void:
 	pass
 
-
+# 工具提示
 func get_default_tooltip() -> String:
 	if upgraded && !upgraded_tooltip_text.is_empty():
 		return upgraded_tooltip_text
 	return tooltip_text
 
-
 func get_updated_tooltip(_player_modifiers: ModifierHandler, _enemy_modifiers: ModifierHandler) -> String:
 	return get_default_tooltip()
-	
